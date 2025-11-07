8c00294310ac940e8bb450bbe7d5cfa69ac8cd17
maskikuy <kproject305@gmail.com>
2025-11-07 09:52:44 +0700

    membuat fungsi tampil data

--- app/src/main/java/com/example/tugaspraktikum/view/TampilData.kt
+++ app/src/main/java/com/example/tugaspraktikum/view/TampilData.kt
@@ -1,2 +1,12 @@
 package com.example.tugaspraktikum.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun TampilData(navController: NavHostController, viewModel: Any){
+    val pesertaList = stringResource(id = R.array.data_peserta)
+}
