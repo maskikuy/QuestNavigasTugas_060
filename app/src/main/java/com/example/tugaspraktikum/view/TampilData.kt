721cf377ea9c51acc43fdb0c9a79adfe44f4df57
maskikuy <kproject305@gmail.com>
2025-11-07 10:18:10 +0700

    memperbaiki code

--- app/src/main/java/com/example/tugaspraktikum/view/TampilData.kt
+++ app/src/main/java/com/example/tugaspraktikum/view/TampilData.kt
@@ -19,58 +19,58 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.tugaspraktikum.R
 
-
 @Composable
-fun TampilData(navController: NavHostController, viewModel: Any){
-    val pesertaList = stringResource(id = R.array.data_peserta)
+fun TampilData(navController: NavHostController, viewModel: Any) {
+    val pesertaList = stringArrayResource(id = R.array.data_peserta)
     val gradient = Brush.verticalGradient(
         colors = listOf(Color(0xFF313647), Color(0xFF1A3D64))
     )
-    Surface (modifier = Modifier.fillMaxSize()){
-        Column (
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
             modifier = Modifier
                 .background(gradient)
                 .fillMaxSize()
-                .padding(15.dp),
+                .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
+        ) {
             Text(
                 text = "List Daftar Peserta",
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.White
             )
+
             Spacer(modifier = Modifier.height(20.dp))
 
-            pesertaList.forEach { peserta->
-                Card (
+            pesertaList.forEach { peserta ->
+                Card(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(vertical = 6.dp),
                     shape = RoundedCornerShape(16.dp),
                     elevation = CardDefaults.cardElevation(8.dp)
-                ){
+                ) {
                     Column(modifier = Modifier.padding(16.dp)) {
                         Text(text = peserta, fontSize = 16.sp)
                     }
                 }
+
                 Spacer(modifier = Modifier.height(20.dp))
 
                 Button(
-                    onClick = { navController.navigate("Formulir")},
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    onClick = { navController.navigate("WelcomeScreen") },
                     modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp)
                 ) {
                     Text("Beranda")
                 }
-                Spacer(modifier = Modifier.height(23.dp))
+                Spacer(modifier = Modifier.height(12.dp))
 
                 Button(
                     onClick = { navController.navigate("Formulir") },
