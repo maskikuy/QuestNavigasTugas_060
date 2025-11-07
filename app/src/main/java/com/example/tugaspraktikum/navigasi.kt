50508635feb51a8a5d1221984032304fb7f2dd7e
maskikuy <kproject305@gmail.com>
2025-11-07 07:45:30 +0700

    menambahkan host nya kedalam fungsi data

--- app/src/main/java/com/example/tugaspraktikum/navigasi.kt
+++ app/src/main/java/com/example/tugaspraktikum/navigasi.kt
@@ -3,6 +3,7 @@ package com.example.tugaspraktikum
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 
@@ -16,5 +17,18 @@ enum class navigasi {
 fun DataApp(
     navController: NavHostController = rememberNavController()
 ){
-
+    NavHost(
+        navController = navController,
+        startDestination = navigasi.WelcomeScreen.name
+    ) {
+        composable(route = navigasi.WelcomeScreen.name) {
+            WelcomeScreen(navController = navController)
+        }
+        composable(route = navigasi.Formulir.name) {
+            Formulir(navController = navController, viewModel = viewModel())
+        }
+        composable(route = navigasi.TampilData.name) {
+            TampilData(navController = navController, viewModel = viewModel())
+        }
+    }
 }
