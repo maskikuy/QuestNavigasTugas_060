499c519c2978edbcf52ce1b328ceff0016f095e6
maskikuy <kproject305@gmail.com>
2025-11-07 09:57:22 +0700

    menambahkan column

--- app/src/main/java/com/example/tugaspraktikum/view/TampilData.kt
+++ app/src/main/java/com/example/tugaspraktikum/view/TampilData.kt
@@ -1,12 +1,17 @@
 package com.example.tugaspraktikum.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import com.example.tugaspraktikum.R
@@ -19,6 +24,14 @@ fun TampilData(navController: NavHostController, viewModel: Any){
         colors = listOf(Color(0xFF313647), Color(0xFF1A3D64))
     )
     Surface (modifier = Modifier.fillMaxSize()){
+        Column (
+            modifier = Modifier
+                .background(gradient)
+                .fillMaxSize()
+                .padding(15.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
 
+        }
     }
 }
