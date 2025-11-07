e7ebd009ea0ab877be02922c74f8deccb55d769f
maskikuy <kproject305@gmail.com>
2025-11-07 09:55:05 +0700

    menambahkan val kedalam fungsi tampil data

--- app/src/main/java/com/example/tugaspraktikum/view/TampilData.kt
+++ app/src/main/java/com/example/tugaspraktikum/view/TampilData.kt
@@ -1,12 +1,19 @@
 package com.example.tugaspraktikum.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
+import com.example.tugaspraktikum.R
 
 
 @Composable
 fun TampilData(navController: NavHostController, viewModel: Any){
     val pesertaList = stringResource(id = R.array.data_peserta)
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Color(0xFF313647), Color(0xFF1A3D64))
+    )
+
 }
